500f2aa34a4db363ce05b0b51b01cefe519a0ca6
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-07-09 12:45:11 -0700

    Finished MVP

--- interview_answers.md
+++ interview_answers.md
@@ -3,8 +3,20 @@ Be prepared to demonstrate your understanding of this week's concepts by answeri
 
 1. What are the main differences between a stateful and a functional component?
 
+Stateful components store data which will re-render whenever it is changed.
+Functional components render JSX and cannot be changed. 
+
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
+
+componentWillMount is called when a component is ready to render for the first time.
+componentWillUpdate is called when a component needs to re-render and update.
 
 3. Define stateful logic.
 
+Stateful logic is logic that is built into a component. Stateful logic uses state that can handle click events, set toggle states, or format data before it gets displayed. 
+
 4. What are the three step of creating a successful test? What is done in each phase?
+
+Arrange - Arranges inputs and targets. Set up the test case
+Act - Acts on the target behavior.
+Assert - Verify your expected outcomes.
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/Checkout Form/i);
+
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+
+    const firstName = screen.getByLabelText(/first name:/i);
+    const lastName = screen.getByLabelText(/last name:/i);
+    const address = screen.getByLabelText(/address:/i);
+    const city = screen.getByLabelText(/city:/i);
+    const state = screen.getByLabelText(/state:/i);
+    const zipCode = screen.getByLabelText(/zip:/i);
+    const checkoutButton = screen.getByRole('button');
+
+    userEvent.type(firstName, 'Alejandro');
+    userEvent.type(lastName, 'Vasquez');
+    userEvent.type(address, '1221 Busher Dr');
+    userEvent.type(city, 'Norstown');
+    userEvent.type(state, 'Wakanda');
+    userEvent.type(zipCode, '43698');
+    userEvent.click(checkoutButton);
+
+    const successMessage = document.querySelector('.success-message');
+    
+    expect(successMessage).toBeInTheDocument();
+});
